018f0344f5a3ef680b8f18ff906598b32134498c
Gabriel Gontijo <96489350+Gonteejo@users.noreply.github.com>
2023-09-13 21:44:56 -0300

    Adiciona eventos de mouseover e mouseout aos produtos

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll(".produto").forEach(elem => {
-        elem.addEventListener("mouseover", () => {
-            elem.classList.add("produto--destaque");
+    //querySelectorAll retorna uma lista de elementos que atendem ao seletor de classe .produto.
+    //forEach percorre a lista de elementos e adiciona os event listeners para cada elemento.
+    //addEventListener adiciona um event listener para o evento de mouseover e mouseout.
+    //assim ao passar o mouse sobre o elemento, a classe produto--destaque é adicionada e ao retirar o mouse, a classe é removida.
+    document.querySelectorAll(".produto").forEach(elemento => {
+        elemento.addEventListener("mouseover", () => {
+            elemento.classList.add("produto--destaque");
         })
-        elem.addEventListener("mouseout", () => {
-            elem.classList.remove("produto--destaque");
+        elemento.addEventListener("mouseout", () => {
+            elemento.classList.remove("produto--destaque");
         })
     });
 });
